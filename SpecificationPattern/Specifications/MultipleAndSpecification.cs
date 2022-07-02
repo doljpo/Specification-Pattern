da243c8b86c1965378abf2b94dcb4feb5de168e8
David Oliveira <dol.jpo@hotmail.com>
2022-07-02 12:35:06 -0300

    Add a specification that accepts multiple criterias, return the objs where all is satisfied

---
+++ SpecificationPattern/Specifications/MultipleAndSpecification.cs
@@ -0,0 +1,21 @@
+﻿using SpecificationPattern.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpecificationPattern.Specifications
+{
+    public class MultipleAndSpecification : ISpecification<Product>
+    {
+        IList<ISpecification<Product>> specifications;
+
+        public MultipleAndSpecification(IList<ISpecification<Product>> specifications)
+        {
+            this.specifications = specifications;
+        }
+
+        public bool IsSatisfied(Product t)
+        {
+            return specifications.All(s => s.IsSatisfied(t));
+        }
+    }
+}
